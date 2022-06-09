83b98d445e96516193b020696ba36b67848ba4c7
Ximanta <ximanta.sarma@gmail.com>
2022-06-09 18:02:13 +0530

    Implemented Ocelot API Gateway to route request to HotelListing.API Service. Closes #14

---
+++ APIGateway/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+/*Load ocelot.json for route configuration*/
+builder.Configuration.AddJsonFile("ocelot.json");
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+/*Registered the 'Ocelot' middleware.*/
+app.UseOcelot().Wait();
+app.Run();
+
---
+++ APIGateway/README.md
@@ -0,0 +1,10 @@
+﻿#14 Create and configure Ocelot API Gateway
+	Use NuGET Manager to install dependency of Ocelot
+	Create ocelot.JSON to define routes.
+		The DownstreamPathTemplate, DownstreamScheme and DownstreamHostAndPorts define the URL that a request will be forwarded to.
+		The UpstreamPathTemplate is the URL that Ocelot will use to identify which DownstreamPathTemplate to use for a given request. 
+        Load ocelot.json, register ocelot as service, and add ocelot to application pipeline in Program.cs
+	To Test:
+		Run the HotelListing.API Service
+		Run the API Gateway Servicd
+		Access with https://localhost:<API Gateway Port>/customers
