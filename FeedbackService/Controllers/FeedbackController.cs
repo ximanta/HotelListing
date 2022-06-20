c7bd86609fae42fad24ba5baf8ac5c56bfa26086
Prashanth P <Prashanth P@DESKTOP-T8FS2FU>
2022-06-20 15:09:43 +0530

    Created a new service with mongo in background. Closes #28

---
+++ FeedbackService/Controllers/FeedbackController.cs
@@ -0,0 +1,74 @@
+﻿using FeedbackService.Model;
+using FeedbackService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeedbackController : ControllerBase
+    {
+
+        private readonly FeedbackServiceImpl _feedbackService;
+
+        public FeedbackController(FeedbackServiceImpl feedbackService) =>
+            _feedbackService = feedbackService;
+
+        [HttpGet]
+        public async Task<List<Feedback>> Get() =>
+            await _feedbackService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Feedback>> Get(string id)
+        {
+            var feedback = await _feedbackService.GetAsync(id);
+
+            if (feedback is null)
+            {
+                return NotFound();
+            }
+
+            return feedback;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Feedback newFeedback)
+        {
+            await _feedbackService.CreateAsync(newFeedback);
+
+            return CreatedAtAction(nameof(Get), new { id = newFeedback.Id }, newFeedback);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Feedback updatedFeedback)
+        {
+            var feedback = await _feedbackService.GetAsync(id);
+
+            if (feedback is null)
+            {
+                return NotFound();
+            }
+
+            updatedFeedback.Id = feedback.Id;
+
+            await _feedbackService.UpdateAsync(id, updatedFeedback);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var feedback = await _feedbackService.GetAsync(id);
+
+            if (feedback is null)
+            {
+                return NotFound();
+            }
+
+            await _feedbackService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ FeedbackService/Model/DatabaseSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace FeedbackService.Model
+{
+    public class DatabaseSetting
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string FeedbackCollectionName { get; set; } = null!;
+    }
+}
